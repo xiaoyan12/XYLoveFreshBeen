7ed3546ce65d64932a1e29652420bf4d1a05dea9
dorayo <yomingyo@gmail.com>
2016-09-18 16:06:21 +0800

    Initial Commit

---
+++ XYLoveFreshBeen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYLoveFreshBeen
+//
+//  Created by 闫世超 on 16/9/18.
+//  Copyright © 2016年 闫世超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYLoveFreshBeen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYLoveFreshBeen
+//
+//  Created by 闫世超 on 16/9/18.
+//  Copyright © 2016年 闫世超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
